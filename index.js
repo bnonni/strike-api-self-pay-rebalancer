c9baeec8acac82cc23cf1e894ce53374738e3dbc
bnonni <me@bryanwnonni.com>
2022-04-30 17:20:59 -0400

    lnd-service

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 #!/usr/bin/env node
 import dotenv from 'dotenv';
-import fetch from 'node-fetch';
-dotenv.config()
+dotenv.config();
 const BASE_URL = process.env.BASE_URL;
 const API_KEY = process.env.API_KEY;
+const TLS_CERT = process.env.TLS_CERT;
+const ADMIN_MACAROON = process.env.ADMIN_MACAROON;
 
-const createInvoices = async (t, m, d) => {
-    let invoices = [];
-    for (let i = t / m; i > 0; i--) {
-        const response = await fetch(`${BASE_URL}/invoices`, {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                Authorization: `Bearer ${API_KEY}`,
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-            redirect: 'follow',
-            body: JSON.stringify({
-                amount: {
-                    amount: m,
-                    currency: 'USD',
-                },
-                description: d,
-            }),
-        });
-        const invoice = await response.json();
-        // console.log(invoice);
-        invoices.push(invoice);
-    }
-    return invoices;
+import fetch from 'node-fetch';
+import lnService from 'ln-service';
+
+const { lnd } = lnService.authenticatedLndGrpc({
+    cert: TLS_CERT,
+    macaroon: ADMIN_MACAROON,
+    socket: '127.0.0.1:10009',
+  });
+  
+
+const api_call = async (path, method, json) => {
+    return await fetch(BASE_URL + path, {
+        method: method,
+        mode: 'cors',
+        headers: {
+            Authorization: `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        redirect: 'follow',
+        body: JSON.stringify(json),
+    });
 };
 
-createInvoices('250', '50', 'rebalance').then(invoices => console.log(invoices));
+const createInvoices = async (total, multiple, currency, memo) => {
+    let invoices = [];  
+    for (let i = total / multiple; i > 0; i--) {
+        const response = await api_call('/invoices', 'POST', {
+            amount: { amount: multiple, currency: currency },
+            description: memo,
+        });
+        const invoice = await response.json();
+        invoices.push(invoice);
+    }
+
+    for (let invoice of invoices) {
+        const response = await api_call(`/invoices/${invoice.invoiceId}/quote`, 'POST');
+        const quote = await response.json()
+        console.log(quote.lnInvoice)
+    }
+};
+
+const invoices = createInvoices('1', '1', 'USD', 'rebalance');
